a1e2e262effe8f71e5e34b51f6a4068b729209c7
juliafsac <juliafs.ac@gmail.com>
2026-04-14 22:10:37 -0500

    atualizando site

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Pesquisa } from './pesquisa/pesquisa';
 import { Menu } from './menu/menu';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Menu],
+  imports: [RouterOutlet, Pesquisa, Menu],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
